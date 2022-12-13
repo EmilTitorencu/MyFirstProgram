c2396caf19ee078547a641053ee02e3f1ceac4aa
Emil Titorencu <Emil.Titorencu@mindgeekconsultants.com>
2022-12-13 10:40:51 +0200

    descriere lab3ex5

--- lab3ex5/Program.cs
+++ lab3ex5/Program.cs
@@ -2,6 +2,16 @@
 
 namespace lab3ex5
 {
+    /* Arpsod adoră două lucruri: matematica și clătitele bunicii sale. Într-o zi, aceasta s-a apucat să
+prepare clătite. Arpsod mănâncă toate clătitele începând de la a N-a clătită preparată, până
+la a M-a clătită preparată (inclusiv N și M). Pentru că el vrea să mănânce clătite cu diferite
+umpluturi și-a făcut următoarea regulă:
+ Dacă numărul de ordine al clătitei este prim atunci aceasta va fi cu ciocolată.
+ Dacă numărul de ordine este pătrat perfect sau cub perfect aceasta va fi cu gem.
+ Dacă suma divizorilor numărului este egală cu însuși numărul de ordine atunci aceasta va fi cu
+înghețată. (se iau în considerare toți divizorii în afară de numărul în sine, inclusiv 1).
+ Dacă niciuna dintre condițiile de mai sus nu este îndeplinită, pentru cele cu numărul de ordine
+par, clătita va fi cu zahar, iar pentru numărul de ordine impar, clătita va fi simplă.” */
     class Program
     {
         static void Main(string[] args)
